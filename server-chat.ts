a9bad651096b663f823b578736c499c07eff3dea
Chanho Kim <kispi@naver.com>
2021-07-19 21:06:45 +0900

    refactor: separated sendMessage out of broadcast

--- server-chat.ts
+++ server-chat.ts
@@ -23,17 +23,22 @@ import { FastifyInstance } from 'fastify'
 
 const connections = {}
 
-const broadcast = (message, req) => {
-  Object.keys(connections).forEach(key => {
-    const connectionWrapper = connections[key]
-    if (!message.v) message.v = {}
-    message.v.u = { n: connectionWrapper.nickname }
-    message.v.isMine = req.headers['sec-websocket-key'] === key
-    if (message.k === 'set_nickname') message.v.l = Object.keys(connections).length
-    if (message.k === 'leave') message.v.l = Object.keys(connections).length - 1
+// req: 웹소켓이 연결된 클라이언트, secWebsocketKey: 메시지를 보낼 대상의 secWebsocketkey
+const sendMessage = ({ message, req, secWebsocketKey }) => {
+  const connectionWrapper = connections[secWebsocketKey]
+  if (!message.v) message.v = {}
 
-    connectionWrapper.connection.socket.send(JSON.stringify(message))
-  })
+  message.v.u = { n: connections[req.headers['sec-websocket-key']].nickname }
+  message.v.isMine = req.headers['sec-websocket-key'] === secWebsocketKey
+  if (message.k === 'set_nickname') message.v.l = Object.keys(connections).length
+  if (message.k === 'leave') message.v.l = Object.keys(connections).length - 1
+
+  connectionWrapper.connection.socket.send(JSON.stringify(message))
+}
+
+// 메시지를 접속된 클라이언트들에게 뿌리고 서버 메모리에 저장한다. (나중에 redis pubsub으로 변경)
+const broadcast = ({ message, req }) => {
+  Object.keys(connections).forEach(secWebsocketKey => sendMessage({ message, req, secWebsocketKey }))
 }
 
 const useChat = (app: FastifyInstance) => {
@@ -52,7 +57,7 @@ const useChat = (app: FastifyInstance) => {
     }))
 
     connection.socket.on('close', () => {
-      broadcast({ k: 'leave' }, req)
+      broadcast({ message: { k: 'leave' }, req })
       delete connections[req.headers['sec-websocket-key']]
     })
 
@@ -61,12 +66,12 @@ const useChat = (app: FastifyInstance) => {
       o.ts = new Date()
       if (o.k === 'set_nickname') {
         connections[req.headers['sec-websocket-key']].nickname = (((o.v || {}) || {}).u || {}).n
-        broadcast(o, req)
+        broadcast({ message: o, req })
         return
       }
 
       if (o.k === 'text') {
-        broadcast(o, req)
+        broadcast({ message: o, req })
         return
       }
     })
