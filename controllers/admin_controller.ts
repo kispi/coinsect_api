1ae25ae4815bda0a60f0ddebca2db557e9c61433
Chris <kispi@naver.com>
2021-09-03 18:01:06 +0900

    fix: move useCRUD to core

--- controllers/admin_controller.ts
+++ controllers/admin_controller.ts
@@ -1,46 +1,15 @@
-import IContext from '../core/context'
-import useService from '../services'
 import { sendMessage } from '../chat/server-chat'
 import { Message } from '../entities/message'
 import { BadWord } from '../entities/bad_word'
-import orm from '../core/orm'
 import { Board } from '../entities/board'
-import store from '../store'
 import { Post } from '../entities/post'
 import { Reaction } from '../entities/reaction'
+import { useCRUD } from '../core/controller'
+import IContext from '../core/context'
+import useService from '../services'
+import store from '../store'
 
 const service = useService()
-
-const useCRUD = model => ({
-  all: (c: IContext) => {
-      orm.querySetter(c, model).getManyAndCount()
-        .then(res => c.res.asJSON({
-          data: res[0],
-          total: res[1],
-        }))
-        .catch(c.res.failed)
-  },
-  detail: (c: IContext) => {
-    orm.querySetter(c, model).where(`id = ${c.req.params['id']}`).getOne()
-      .then(c.res.asJSON)
-      .catch(c.res.failed)
-  },
-  delete: (c: IContext) => {
-    orm.querySetter(c, model).where(`id = ${c.req.params['id']}`).delete().execute()
-      .then(() => c.res.success())
-      .catch(c.res.failed)
-  },
-  create: (c: IContext) => {
-    orm.querySetter(c, model).insert().into(model).values(c.req.body).execute()
-      .then(() => c.res.success())
-      .catch(c.res.failed)
-  },
-  update: (c: IContext) => {
-    orm.querySetter(c, model).update(model).set(c.req.body).where(`id = ${c.req.params['id']}`).execute()
-      .then(() => c.res.success())
-      .catch(c.res.failed)
-  },
-})
 
 const routesChat = {
   banIP: (c: IContext) => {
@@ -78,9 +47,9 @@ const adminController = {
   chat: routesChat,
   store: routesStore,
   badWord: useCRUD(BadWord),
-  board: useCRUD(Board),
+  board: useCRUD(Board, true),
   message: useCRUD(Message),
-  post: useCRUD(Post),
+  post: useCRUD(Post, true),
   reaction: useCRUD(Reaction),
 }
 
---
+++ core/controller.ts
@@ -0,0 +1,35 @@
+import IContext from './context'
+import orm from './orm'
+
+export const useCRUD = (model, useSoftDelete?) => ({
+  all: (c: IContext) => {
+      orm.querySetter(c, model).getManyAndCount()
+        .then(res => c.res.asJSON({
+          data: res[0],
+          total: res[1],
+        }))
+        .catch(c.res.failed)
+  },
+  detail: (c: IContext) => {
+    orm.querySetter(c, model).where(`id = ${c.req.params['id']}`).getOne()
+      .then(c.res.asJSON)
+      .catch(c.res.failed)
+  },
+  delete: (c: IContext) => {
+    const o = orm.querySetter(c, model).where(`id = ${c.req.params['id']}`)
+    const promise = useSoftDelete ? o.softDelete() : o.delete()
+    promise.execute()
+      .then(() => c.res.success())
+      .catch(c.res.failed)
+  },
+  create: (c: IContext) => {
+    orm.querySetter(c, model).insert().into(model).values(c.req.body).execute()
+      .then(() => c.res.success())
+      .catch(c.res.failed)
+  },
+  update: (c: IContext) => {
+    orm.querySetter(c, model).update(model).set(c.req.body).where(`id = ${c.req.params['id']}`).execute()
+      .then(() => c.res.success())
+      .catch(c.res.failed)
+  },
+})
