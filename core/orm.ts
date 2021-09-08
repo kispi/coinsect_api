5118c3c3f46b037234aa2ab9c501a8b027d6fe38
Chris <kispi@naver.com>
2021-09-08 12:25:08 +0900

    feature: make querybuilder possible to request join

--- core/orm.ts
+++ core/orm.ts
@@ -1,13 +1,27 @@
+import { getRepository } from 'typeorm'
 import IContext from './context'
+
+const columnWithTable = (column, entityName) => {
+  if (column.includes('.')) return column
+
+  return `${entityName}.${column}`
+}
 
 const orm = {
   querySetter: (c: IContext, model) => {
     const q = c.req.query
     const qb = c.orm.getRepository(model).createQueryBuilder()
+
+    const entityName = getRepository(model).metadata.name
     if (q['limit']) qb.limit(q['limit'])
     if (q['offset']) qb.offset(q['offset'])
-    if (q['sort']) qb.orderBy(q['sort'], (q['order'] || 'desc').toUpperCase())
+    if (q['sort']) qb.orderBy(columnWithTable(q['sort'], entityName), (q['order'] || 'desc').toUpperCase())
     if (q['where']) qb.where(decodeURI(q['where']))
+    if (q['join']) {
+      q['join'].split(',').forEach(foreignEntityName => {
+        qb.leftJoinAndSelect(`${entityName}.${foreignEntityName}`, foreignEntityName)
+      })
+    }
     return qb
   },
 }
