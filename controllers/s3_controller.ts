e2f77b4878953e2b0886e0df0c3e3eb8524d7e4a
Chris <kispi@naver.com>
2021-11-29 18:23:21 +0900

    bugfix: getSignedUrl didn't respond to user for a very long time wtf...

--- controllers/s3_controller.ts
+++ controllers/s3_controller.ts
@@ -4,11 +4,18 @@ import useService from '../services'
 const services = useService()
 
 const s3Controller = {
-  getSignedUrl: (c: IContext) => services.s3.getSignedUrl({
-    key: c.req.query['key'] || '',
-    tagging: c.req.query['tagging'] || '',
-    nouuid: c.req.query['noUuid'],
-  }),
+  getSignedUrl: async (c: IContext) => {
+    try {
+      const url = await services.s3.getSignedUrl({
+        key: c.req.query['key'] || '',
+        tagging: c.req.query['tagging'] || '',
+        nouuid: c.req.query['noUuid'],
+      })
+      c.res.success(url)
+    } catch (e) {
+      c.res.failed(e)
+    }
+  },
   deleteObjectTagging: (c: IContext) => services.s3.deleteObjectTagging(c.req.body['key']),
   deleteObject: (c: IContext) => services.s3.deleteObject(c.req.body['key']),
 }
