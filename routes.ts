52f5be21f6d03b0b074362adb74f943a742b536c
Chris <kispi@naver.com>
2022-04-01 17:25:50 +0900

    fix: remove authorization for deploy apis

--- routes.ts
+++ routes.ts
@@ -26,11 +26,11 @@ export const useRoutes = (app: FastifyInstance) => ({
   deploy: () => {
     const router = useRouter(app)
 
-    router.get('/deploy/coinsect_web', ctrls.deploy['coinsect_web'].status, auth.admin)
-    router.post('/deploy/coinsect_web', ctrls.deploy['coinsect_web'].request, auth.admin)
+    router.get('/deploy/coinsect_web', ctrls.deploy['coinsect_web'].status)
+    router.post('/deploy/coinsect_web', ctrls.deploy['coinsect_web'].request)
 
-    router.get('/deploy/coinsect_api', ctrls.deploy['coinsect_api'].status, auth.admin)
-    router.post('/deploy/coinsect_api', ctrls.deploy['coinsect_api'].request, auth.admin)
+    router.get('/deploy/coinsect_api', ctrls.deploy['coinsect_api'].status)
+    router.post('/deploy/coinsect_api', ctrls.deploy['coinsect_api'].request)
   },
   admin: () => {
     const router = useRouter(app)
