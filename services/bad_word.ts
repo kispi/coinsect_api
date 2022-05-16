87d084cda22a0f42a87d24d3d5f3d3720e23a5ce
Chris <kispi@naver.com>
2022-05-16 15:52:26 +0900

    fix: filter should filter same bad word for the entire message

--- services/bad_word.ts
+++ services/bad_word.ts
@@ -4,7 +4,10 @@ export default {
   includedIn: (message: string) => store.state.badWords.map(o => o.word).some(badWord => message.includes(badWord)),
   filtered: (message: string) => {
     let filtered = message
-    store.state.badWords.forEach(badWord => filtered = filtered.replace(badWord.word, badWord.alternative || '*'.repeat(badWord.word.length)))
+    store.state.badWords.forEach(badWord => {
+      const regex = new RegExp(badWord.word, 'gi')
+      filtered = filtered.replace(regex, badWord.alternative || '*'.repeat(badWord.word.length))
+    })
     return filtered
   },
 }
