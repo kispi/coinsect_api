f45b216adaf298d48bbde6d1b5d118f5d8a0a3a5
Chris <kispi@naver.com>
2022-05-25 10:30:04 +0900

    fix: wrong migration

--- entities/post.ts
+++ entities/post.ts
@@ -30,7 +30,7 @@ export class Post extends BaseModel {
   content: string
 
   @Column({ default: TypePostType.Normal })
-  TypePostType: TypePostType
+  PostType: TypePostType
 
   @Column({ default: 0 })
   views: number
