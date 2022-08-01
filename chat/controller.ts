626b5f62e6a0e3927ef8e14f128104f522d76ba1
Chris <kispi@naver.com>
2022-08-01 16:38:28 +0900

    fix: try until it works

--- chat/controller.ts
+++ chat/controller.ts
@@ -86,7 +86,11 @@ export const chatCtrl = {
         if (q['sort']) {
           const sign = q['order'] === 'desc' ? -1 : 1
           const c = q['sort']
-          filtered.sort((a, b) => (a[c] || null) > (b[c] || null) ? sign : -sign) // prevent comparing on undefined
+          filtered.sort((a, b) => {
+            if (!a[c] && b[c]) return -sign
+
+            if (a[c] && b[c]) return a[c] > b[c] ? sign : -sign
+          })
         }
 
         c.res.asJSON({
