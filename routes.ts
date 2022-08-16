71db46678aa62ca7efd88fa70a02361d144676ec
Chris <kispi@naver.com>
2022-08-16 20:24:16 +0900

    fix: GET real_time_positions now is only for admin

--- routes.ts
+++ routes.ts
@@ -50,6 +50,7 @@ export const useRoutes = (app: FastifyInstance) => ({
 
     router.get('/admin/contents/real_time_positions/change_notifications', ctrls.content.realTimePositions.changeNotification.all, auth.admin.position)
     router.get('/admin/contents/real_time_positions/presets', ctrls.content.realTimePositions.presets, auth.admin.position)
+    router.get('/admin/contents/real_time_positions', ctrls.content.realTimePositions.all, auth.admin.position)
     router.post('/admin/contents/real_time_positions', ctrls.content.realTimePositions.set, auth.admin.position)
     router.delete('/admin/contents/real_time_positions/:id', ctrls.content.realTimePositions.delete, auth.admin.position)
 
@@ -103,7 +104,6 @@ export const useRoutes = (app: FastifyInstance) => ({
     router.get('/market_info/assets_including_metal', ctrls.marketInfo.assetsIncludingMetal)
 
     router.get('/contents/public_treasuries', ctrls.content.publicTreasuries)
-    // router.get('/contents/real_time_positions', ctrls.content.realTimePositions.all) // 부하 줄이기 위해 임시 서빙 중지
     router.post('/contents/real_time_positions/change_notifications', ctrls.content.realTimePositions.changeNotification.create)
 
     router.get('/contents/news/cobak/feeds', ctrls.content.news.cobak.feeds)
