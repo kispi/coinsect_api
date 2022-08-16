ef43accde028580512209a2d3a1feea85bd653bb
Chris <kispi@naver.com>
2022-08-16 10:24:18 +0900

    fix: add KLAY, GUSD, PAX, YFI to symbols.ts

--- constants/symbols.ts
+++ constants/symbols.ts
@@ -1,34 +1,22 @@
 import helpers from '../core/helpers'
 
-export default {
-  USDT: {
-    symbol: 'USDT',
-    thumb: helpers.useCdn('images/symbols/USDT.png'),
-    kr: '테더',
-    en: 'Tether',
-  },
-  BNB: {
-    symbol: 'BNB',
-    thumb: helpers.useCdn('images/symbols/BNB.png'),
-    kr: '바이낸스 코인',
-    en: 'Binance Coin',
-  },
-  BUSD: {
-    symbol: 'BUSD',
-    thumb: helpers.useCdn('images/symbols/BUSD.png'),
-    kr: '바이낸스 USD',
-    en: 'Binance USD',
-  },
-  USDC: {
-    symbol: 'USDC',
-    thumb: helpers.useCdn('images/symbols/USDC.png'),
-    kr: 'USD 코인',
-    en: 'USD Coin',
-  },
-  WBTC: {
-    symbol: 'WBTC',
-    thumb: helpers.useCdn('images/symbols/WBTC.png'),
-    kr: '랩드 비트코인',
-    en: 'Wrapped Bitcoin',
-  },
-}
+const arr = [
+  { symbol: 'USDT', kr: '테더', en: 'Tether' },
+  { symbol: 'BNB', kr: '바이낸스 코인', en: 'Binance Coin' },
+  { symbol: 'BUSD', kr: '바이낸스 USD', en: 'Binance USD' },
+  { symbol: 'USDC', kr: 'USD 코인', en: 'USD Coin' },
+  { symbol: 'WBTC', kr: '랩드 비트코인', en: 'Wrapped Bitcoin' },
+  { symbol: 'KLAY', kr: '클레이튼', en: 'Klaytn' },
+  { symbol: 'GUSD', kr: '제미니 USD', en: 'Gemini USD' },
+  { symbol: 'PAX', kr: '팩소스 스탠다드', en: 'Paxos Standard' },
+  { symbol: 'YFI', kr: '연 파이낸스', en: 'Yearn Finance' },
+]
+
+const o = {}
+arr.forEach(item => o[item.symbol] = {
+  symbol: item.symbol,
+  kr: item.kr,
+  en: item.en,
+  thumb: helpers.useCdn(`images/symbols/${item.symbol}.png`)
+})
+export default o
