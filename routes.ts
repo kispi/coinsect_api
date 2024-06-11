32c72cf205dc42f1fb304eefb2eac0d54e2cd88f
Chris <kispi@naver.com>
2024-06-11 11:24:55 +0900

    fix: add missing deploy controller

--- routes.ts
+++ routes.ts
@@ -30,6 +30,9 @@ export const useRoutes = (app: FastifyInstance) => ({
 
     router.get('/deploy/coinsect_api', ctrls.deploy['coinsect_api'].status)
     router.post('/deploy/coinsect_api', ctrls.deploy['coinsect_api'].request)
+
+    router.get('/deploy/remix', ctrls.deploy['remix'].status)
+    router.post('/deploy/remix', ctrls.deploy['remix'].request)
   },
   admin: () => {
     const router = useRouter(app)
