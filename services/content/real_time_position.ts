d9af0eaf3928cf9d70c8776542cbec1f00679f43
kispi <kispi@naver.com>
2025-09-23 11:36:28 +0900

    fix: try gemini-2.5-flash

--- services/content/real_time_position.ts
+++ services/content/real_time_position.ts
@@ -243,7 +243,7 @@ const realTimePositionService = {
   }) => {
     const genAI = new GoogleGenerativeAI(store.state.serverConfig.GOOGLE_AI_STUDIO)
     const model = genAI.getGenerativeModel({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-2.5-flash',
       generationConfig: {
         responseMimeType: 'application/json',
       },
